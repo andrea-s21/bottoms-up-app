3e087ed9d52e46f9f7ed15a35e027288bdb01c06
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-02-18 08:52:22 -0800

    required mixin

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin 
 from .models import ANSWERS1, Category, Drink, Ingredient, Survey, Photo
 from django.contrib.auth.models import User
 import requests, random 
